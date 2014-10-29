c63c6c69d4554b914939c730055e255cf55bc749
travellife <1025263614@qq.com>
2014-10-29 14:32:22 +0800

    测试修改

--- src/com/lsj/alarm/util/AppConstant.java
+++ src/com/lsj/alarm/util/AppConstant.java
@@ -3,7 +3,7 @@ package com.lsj.alarm.util;
 public class AppConstant {
 
 	public static class Action {
-		// 自定义闹钟接收广播
+		// 自定义闹钟接收广播 啦啦啦
 		public static final String ALARM_RECEIVER = "android.intent.action.ALARM_RECEIVER";
 	}
 }
